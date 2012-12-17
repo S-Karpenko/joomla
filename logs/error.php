4d4f4d86bef3beceb8d7a54b3fa2837eb073c0d1
S-Karpenko <s.anatolievi4@gmail.com>
2012-12-18 01:45:39 +0200

    install JEvents+calendar, create Заходи, News on Головна, config. modules, config. user, config menu, config joomla

---
+++ logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2012-12-15 22:08:20 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2012-12-15	22:08:20	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2012-12-16	22:33:36	INFO	127.0.0.1	Joomla CANCELED: 	
+2012-12-16	22:33:46	INFO	127.0.0.1	Joomla CANCELED: 	
+2012-12-16	22:35:07	INFO	127.0.0.1	Joomla CANCELED: 	
+2012-12-16	22:35:49	INFO	127.0.0.1	Joomla FAILURE: 	Empty password not allowed
